5c2d06ebf49bff2c168f01e24f8c6af69fe2b545
Elzero Rifay <elzerorifay@gmail.com>
2022-09-13 11:38:27 +0200

    Color Mixer with API Using JavaScript

--- java.js
+++ java.js
@@ -4,36 +4,33 @@ function chagneColor() {
   document.body.style.backgroundColor = "#" + RandomColor;
   resultInHexa.innerHTML = "#" + RandomColor;
   console.log("#" + RandomColor);
-  const red = document.getElementById("red");
-  const green = document.getElementById("green");
-  const blue = document.getElementById("blue");
-  red.value = "#" + RandomColor;
-  green.value = "#" + RandomColor;
-  blue.value = "#" + RandomColor;
 
-}
+  }
+
+  
 
 
+function moveColors() {
+const red = parseInt(document.getElementById("red").value);
+const green = parseInt(document.getElementById("green").value);
+const blue = parseInt(document.getElementById("blue").value);
+const colors = "#" + hex(red) + hex(green) + hex(blue);
 
 
-// function chagneColor() {
-// const red = parseInt(document.getElementById("red").value);
-// const green = parseInt(document.getElementById("green").value);
-// const blue = parseInt(document.getElementById("blue").value);
-// const colors = "#" + hex(red) + hex(green) + hex(blue);
+  function hex(v) {
+    var hex = v.toString(16);
+    if (v < 16) {
+      hex = "0" + hex;
+    }
+    return hex;
+  }
 
-//   function hex(v) {
-//     var hex = v.toString(16);
-//     if (v < 16) {
-//       hex = "0" + hex;
-//     }
-//     return hex;
-//   }
-//   document.body.style.backgroundColor = colors;
-//   document.getElementById("resultInHexa").innerHTML = colors;
-//   console.log("Done!");
-// }
+   document.body.style.backgroundColor = colors;
+     resultInHexa.innerHTML = colors;
 
-// document.getElementById('red').addEventListener("onmousemove", chagneColor)
-// document.getElementById('green').addEventListener("onmousemove", chagneColor())
-// document.getElementById('blue').addEventListener("onmousemove", chagneColor())
+    }
+
+
+// document.getElementById('red').addEventListener("mousemove", chagneMove)
+// document.getElementById('green').addEventListener("mousemove", chagneMove )
+// document.getElementById('blue').addEventListener("mousemove", chagneMove)
